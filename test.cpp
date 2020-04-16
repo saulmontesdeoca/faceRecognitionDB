d8d69a6ffd4e670816ecd20899a8228b4769a42a
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-04-16 00:12:49 -0500

    Object Oriented alike

--- test.cpp
+++ test.cpp
@@ -3,104 +3,30 @@
 #include <vector>
 #include <cstdint>
 #include <mongocxx/uri.hpp>
-// #include <opencv2/core/mat.hpp>
-
+#include <opencv2/core.hpp>
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/json.hpp>
 #include <mongocxx/stdx.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+#include "db.hpp"
 
-
+using namespace cv;
 using namespace std;
-// Creando la conexion al cluster
-mongocxx::instance instance{};
-mongocxx::client conn{mongocxx::uri{"mongodb+srv://saul-admin:Saul098923.23@cluster0-f1rg5.mongodb.net/test?retryWrites=true&w=majority"}};
-
-//Seleccionando la base de datos Cuatecs
-mongocxx::database db = conn["cuatecs"];
-
-//Seleccionando la coleccion
-mongocxx::collection coll = db["subjects"];
-
-//Create
-void create(string nombre, int edad, string matricula, string imgURL){
-    auto builder = bsoncxx::builder::stream::document{};
-    bsoncxx::document::value doc_value = builder
-    << "nombre" << nombre
-    << "imgURL" << imgURL
-    << "edad" << edad
-    << "matricula" << matricula
-    << "identificacionFacial" << bsoncxx::builder::stream::open_document
-        << "rostro1" << bsoncxx::builder::stream::open_array
-            << 32 << 54 << 87 << -33 << 12
-            << bsoncxx::builder::stream::close_array
-        << "rostro2" << bsoncxx::builder::stream::open_array
-            << 32 << 54 << 87 << -33 << 12
-            << bsoncxx::builder::stream::close_array
-        << "rostro3" << bsoncxx::builder::stream::open_array
-            << 32 << 54 << 87 << -33 << 12
-            << bsoncxx::builder::stream::close_array
-    << bsoncxx::builder::stream::close_document
-    << bsoncxx::builder::stream::finalize;
-
-    bsoncxx::stdx::optional<mongocxx::result::insert_one> result = 
-    coll.insert_one(doc_value.view()); //Inserting Document
-    cout << "Inserted " << result->inserted_id().get_oid().value.to_string() << endl;
-
-    //cout << result->inserted_id().get_int64() << "\n";
-}
-//Read
-void readAll(){
-    mongocxx::cursor cursor = coll.find({});
-    for(auto doc : cursor) {
-    std::cout << bsoncxx::to_json(doc) << "\n";
-    }
-}
-void readOne(string matricula){
-    bsoncxx::stdx::optional<bsoncxx::document::value> doc =
-    coll.find_one(bsoncxx::builder::stream::document{} << "matricula" << matricula
-    << bsoncxx::builder::stream::finalize);
-    cout << bsoncxx::to_json(doc->view()) << "\n";
-}
-//Read with id
-// void readOne(string id){
-//     //bsoncxx::oid oid = bson[id].get_oid().value;
-//     //bsoncxx::oid myoid(accountId);
-//     bsoncxx::stdx::optional<bsoncxx::document::value> doc =
-//     coll.find_one(bsoncxx::builder::stream::document{} << "nombre" << id << bsoncxx::builder::stream::finalize);
-//     cout << bsoncxx::to_json(doc->view()) << "\n";
-// }
-
-//Update
-void updateName(string nombre, string nuevoNombre){
-    coll.update_one(bsoncxx::builder::stream::document{} << "nombre" << nombre << bsoncxx::builder::stream::finalize,
-                      bsoncxx::builder::stream::document{} << "$set" << bsoncxx::builder::stream::open_document <<
-                        "nombre" << nuevoNombre << bsoncxx::builder::stream::close_document 
-                        << bsoncxx::builder::stream::finalize);
-}
-void updateMatricula(string matricula, string nuevaMatricula){
-    coll.update_one(bsoncxx::builder::stream::document{} << "matricula" << matricula << bsoncxx::builder::stream::finalize,
-                      bsoncxx::builder::stream::document{} << "$set" << bsoncxx::builder::stream::open_document <<
-                        "matricula" << nuevaMatricula << bsoncxx::builder::stream::close_document 
-                        << bsoncxx::builder::stream::finalize);
-}
-//Delete
-void deleteOne(string matricula){
-    coll.delete_one(bsoncxx::builder::stream::document{} << "matricula" << matricula << bsoncxx::builder::stream::finalize);
-}
  
 int main(int, char**) {
 
+    DataBase db;
+
     //Create
-    //create("Luis Garcia", 21, "A01021111", "https://www.thispersondoesnotexist.com/image");
+    db.create("Hola", 23, "A01099911", "https://www.thispersondoesnotexist.com/image", Mat::zeros( 4, 4, CV_32FC2 ));
     //Read
-    //readAll();
-    //readOne("A01021111");
+    //db.readAll();
+    //db.readOne("A01021111");
     //Update
-    //updateName("Luis Garcia ","Victor Coeto");
-    // updateMatricula("A01021111", "A01025986");
+    //db.updateName("Luis Garcia ","Victor Coeto");
+    //db.updateMatricula("A01021111", "A01025986");
     //Delete
-    //deleteOne("Victor Coeto");
+    //ddb.eleteOne("Victor Coeto");
 
 }
